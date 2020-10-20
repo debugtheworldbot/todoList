729e702693e8ede9f1dc6a699bae2b96041553b4
田成壮 <istiancz@gmail.com>
2020-10-20 17:11:05 +0800

    reset

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import {useState} from "react";
 
 function App() {
     const [todos, setTodos] = useState([])
+    const toggleComplete = (i) => {
+        setTodos(todos.map((todo, k) => i === k ? {...todo,complete: !todo.complete} : todo))
+    }
+
 
     return (
         <div className="App">
             <Form onSubmit={(text) => setTodos([{text, complete: false}, ...todos])}/>
-            {todos.map(({text,complete}) => (<div key={text} >{text}</div>))}
+            <div>
+                {todos.map(({text, complete}, i) =>
+                    <div style={{textDecoration: complete ? 'line-through' : ''}} key={i}
+                         onClick={() => toggleComplete(i)}>{text}</div>)}
+            </div>
+            <button onClick={()=>setTodos([])}> reset</button>
         </div>
     );
 }
